d72c62b8d4fe2268004a9c8086686d4dea358629
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2023-12-26 19:24:54 +0530

    mongodb questions

--- endgamept-2/routes/index.js
+++ endgamept-2/routes/index.js
@@ -19,12 +19,23 @@ router.get('/creates',async (req,res)=>{
   // res.send(userdata);
 })
 
-router.get('/find', async (req,res)=>{
-  let regax = new RegExp("^Dnyanesh$","i");//it will check insensitively the username
-  let user = await userModel.find({username:regax});
-  res.send(user)
-})
+// router.get('/find', async (req,res)=>{
+//   let regax = new RegExp("^Dnyanesh$","i");//it will check insensitively the username
+//   let user = await userModel.find({username:regax});
+//   res.send(user)
+// })
 
+// router.get('/find',async (req,res)=>{
+//  let user = await userModel.find({categories :{$all:["react"]}})
+//  res.send(user)
+// })
+
+router.get('/find',async (req,res)=>{
+  let date1 = new Date('2023-11-02')
+  let date2 = new Date('2023-11-03')
+  let user = await userModel.find({datecreated:{$gte:date1 , $lte:date2}})
+  res.send(user)
+});
 // router.get('/failed',(req,res)=>{
 // req.flash("age",12)
 // req.flash("name","Dnyanesh")
